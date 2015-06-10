dc562452b46977ab0c220aa88a3cd4560356cf62
andre-cabral <cabral.dev@gmail.com>
2015-06-10 01:52:15 -0300

    Menu principal move do menu para options e de options para o menu.

---
+++ Assets/Scripts/MainMenu/MenuTransitions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTransitions : MonoBehaviour {
+	Animator animatorObject;
+	HashAnimatorMainMenu hashMainMenu;
+
+
+	void Awake(){
+		animatorObject = GetComponent<Animator>();
+		hashMainMenu = GetComponent<HashAnimatorMainMenu>();
+	}
+
+	public void MainToOptions () {
+		animatorObject.SetTrigger(hashMainMenu.mainToOptions);
+	}
+
+	public void OptionsToMain(){
+		animatorObject.SetTrigger(hashMainMenu.optionsToMain);
+	}
+}
---
+++ Assets/Scripts/NamesPatterns/HashAnimatorMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashAnimatorMainMenu : MonoBehaviour {
+
+	public int mainToOptions;
+	public int optionsToMain;
+	
+	void Awake() {
+		mainToOptions = Animator.StringToHash("MainToOptions");
+		optionsToMain = Animator.StringToHash("OptionsToMain");
+	}
+}
