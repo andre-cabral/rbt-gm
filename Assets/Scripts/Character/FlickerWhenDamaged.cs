c1bf0fcbb9ea9650ca0901ea637d3bb26e5f374d
andre-cabral <cabral.dev@gmail.com>
2015-07-30 01:50:12 -0300

    Sistema de vida do jogador

---
+++ Assets/Scripts/Character/FlickerWhenDamaged.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerWhenDamaged : MonoBehaviour {
+
+	public GameObject[] objectsToFlicker;
+	public float timeToDisappear = .2f;
+	public float timeToAppear = .5f;
+	public float timeToFlicker = 5f;
+	private float actualTime = 0f;
+	public float totalTimeFlicker = 0f;
+	private bool areTheObjectsOn = true;
+	public bool flicker = false;
+
+	void Update () {
+
+		if(flicker){
+			if(timeToFlicker >= totalTimeFlicker){
+				//flicker on
+				if(areTheObjectsOn && actualTime <= timeToAppear){
+					actualTime += Time.deltaTime;
+				}
+				if(areTheObjectsOn && actualTime > timeToAppear){
+					actualTime = 0f;
+					TurnObjectsOff();
+				}
+
+				//flicker off
+				if(!areTheObjectsOn && actualTime <= timeToDisappear){
+					actualTime += Time.deltaTime;
+				}
+				if(!areTheObjectsOn && actualTime > timeToDisappear){
+					actualTime = 0f;
+					TurnObjectsOn();
+				}
+				totalTimeFlicker += Time.deltaTime;
+			}else{
+				totalTimeFlicker = 0f;
+				flicker = false;
+
+				TurnObjectsOn();
+			}
+		}
+
+	}
+
+	void TurnObjectsOn(){
+		foreach(GameObject objectToTurnOn in objectsToFlicker){
+			objectToTurnOn.SetActive(true);
+		}
+		areTheObjectsOn = true;
+	}
+	void TurnObjectsOff(){
+		foreach(GameObject objectToTurnOn in objectsToFlicker){
+			objectToTurnOn.SetActive(false);
+		}
+		areTheObjectsOn = false;
+	}
+
+	public void startFlickering(){
+		this.flicker = true;
+		TurnObjectsOff();
+	}
+	public bool getFlicker(){
+		return flicker;
+	}
+}
---
+++ Assets/Scripts/GameController/LifeManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeManager : MonoBehaviour {
+	public int maxLife = 3;
+	public int initialLife = 3;
+	int lifePoints;
+
+	void Awake () {
+		//if there will be persistence on life, get the life here
+		lifePoints = initialLife;
+	}
+
+	public void LifeGain(int gainedLife){
+		if(gainedLife + lifePoints > maxLife){
+			lifePoints = maxLife;
+		}else{
+			lifePoints += gainedLife;
+		}
+	}
+
+	public void LifeLoss (int lifeToLose){
+		lifePoints -= lifeToLose;
+	}
+
+	public int GetLife(){
+		return lifePoints;
+	}
+}
