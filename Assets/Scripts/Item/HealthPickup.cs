0918c73ba2830ddc2518afeb8c5992a245eed826
andre-cabral <cabral.dev@gmail.com>
2015-09-17 15:29:04 -0300

    Health pickup funcionando.

---
+++ Assets/Scripts/Item/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour {
+
+	public int lifeToGain = 1;
+	private LifeManager lifeManager;
+	
+	void Awake(){
+		GameObject gameController = GameObject.FindGameObjectWithTag(Tags.gameController);
+		lifeManager = gameController.GetComponent<LifeManager>();
+	}
+	
+	void OnTriggerStay(Collider col){
+		if(col.gameObject.tag == Tags.characterClass){
+			CollidedWithPlayer();
+		}
+	}
+
+	void CollidedWithPlayer(){
+		if(lifeManager.GetLife() < lifeManager.maxLife){
+			lifeManager.LifeGain(lifeToGain);
+			Destroy(gameObject);
+		}
+	}
+}
