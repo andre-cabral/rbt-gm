f2843a2270561d4941ddde471ae32b551248ae49
andre-cabral <cabral.dev@gmail.com>
2015-10-09 20:20:54 -0300

    Porta da escada de saida abre e fecha por proximidade e pode exigir uma chave para abrir.

---
+++ Assets/Scripts/Item/LadderDoorOpener.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LadderDoorOpener : MonoBehaviour {
+
+	public KeysNames keyNeeded;
+	public string messageIfHasKey;
+	public string messageIfDontHaveKey;
+	public GameObject doorToOpen;
+	Animator animator;
+	HashAnimatorLadderDoor hashAnimatorLadderDoor;
+	private ScreenMessageManager screenMessageManager;
+	private InventoryManager inventory;
+	
+	
+	void Awake(){
+		inventory = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<InventoryManager>();
+		GameObject gameController = GameObject.FindGameObjectWithTag(Tags.gameController);
+		screenMessageManager = gameController.GetComponent<ScreenMessageManager>();
+		animator = doorToOpen.GetComponent<Animator>();
+		hashAnimatorLadderDoor = doorToOpen.GetComponent<HashAnimatorLadderDoor>();
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if( col.gameObject.tag == Tags.characterClass ){
+			if(keyNeeded == KeysNames.greenKey){
+				if( inventory.getHasGreenKey() ){
+					Open();
+				}else{
+					DontHaveTheKeyMessage();
+				}
+			}else if(keyNeeded == KeysNames.noKey){
+				Open();
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if( col.gameObject.tag == Tags.characterClass ){
+			Close();
+		}
+	}
+	
+	void DontHaveTheKeyMessage(){
+		screenMessageManager.NewMessage(messageIfDontHaveKey);
+	}
+	
+	void Open(){
+		if(messageIfHasKey != ""){
+			screenMessageManager.NewMessage(messageIfHasKey);
+		}
+		animator.SetBool(hashAnimatorLadderDoor.opened, true);
+	}
+
+	void Close(){
+		animator.SetBool(hashAnimatorLadderDoor.opened, false);
+	}
+
+}
---
+++ Assets/Scripts/NamesPatterns/HashAnimatorLadderDoor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashAnimatorLadderDoor : MonoBehaviour {
+
+	public int opened;
+	
+	void Awake() {
+		opened = Animator.StringToHash("Opened");
+	}
+}
